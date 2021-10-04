7e498bf5592940386933c7f8934cb50cd25443ca
saira <sairaarif463@gmail.com>
2021-10-04 04:22:30 -0700

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Object_Oriented_Language
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
